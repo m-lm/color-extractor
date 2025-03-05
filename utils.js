ec78010d48a30e5ac47e726648a0d110808821b5
m-lm <31556425+m-lm@users.noreply.github.com>
2025-03-05 13:14:47 -0600

    implement histogram color extraction method based on RGB frequencies

--- utils.js
+++ utils.js
@@ -1,45 +1,96 @@
-function displayColors() {
+function displayColors(colors, k=5) {
     // Display colors after user generation
-    console.log("Display Colors");
+    const numSamples = Math.min(k, colors.length);
+    const display = document.getElementById("colors"); 
+    if (display.childElementCount != numSamples) {
+        clearColors();
+    }
+    if (display.childElementCount <= 0) {
+        for (let i = 0; i < numSamples; i++) {
+            const colorBox = document.createElement("div");
+            colorBox.id = "colorsChildren";
+            colorBox.style.backgroundColor = colors[i][0];
+            display.style.opacity = 100;
+            colorBox.style.width = "24px";
+            colorBox.style.height = "24px";
+            display.appendChild(colorBox);
+        }
+    }
 }
 
-function kMeans(k=3) {
+function clearColors() {
+    // Clear colors that are currently displayed
+    const colors = document.getElementById("colors"); 
+    while (colors.firstChild) {
+        colors.removeChild(colors.firstChild);
+    }
+}
+
+function kMeans(image, k=5) {
     // Color extraction using K-Means Clustering
     console.log("K Means Clustering");
 }
 
-function medianCut() {
+function medianCut(image, k=5) {
     // Color extraction using Median Cut
     console.log("Median Cut");
 }
 
-function freqAnalysis() {
-    // Color extraction using Frequency Analysis
-    console.log("Frequency Analysis");
+function freqAnalysis(image, k=5) {
+    // Color extraction using color frequencies
+    k = Math.min(k, 10);
+    const pixels = image.data;
+    const colors = {};
+    const stepConstant = 256 / k;
+    const quantize = (value, step=stepConstant) => Math.floor(value / step) * step;
+
+    for (let i = 0; i < pixels.length; i += 4) {
+        let r = quantize(pixels[i]), g = quantize(pixels[i+1]), b = quantize(pixels[i+2]);
+        const key = `rgb(${r}, ${g}, ${b})`;
+        if (colors[key]) {
+            colors[key]++;
+        }
+        else {
+            colors[key] = 1;
+        }
+    }
+    const sortedColors = Object.entries(colors).sort((value1, value2) => value2[1] - value1[1]);
+    displayColors(sortedColors, k);
 }
 
 function generateColors() {
     // Function that calls methods for color extraction accordingly
     const choice = document.getElementById("method-choice").value;
+    const k = parseInt(document.getElementById("quantity").value);
     const display = document.getElementById("img-display");
-    if (display.src) {
-        switch (choice) {
-            case "K-Means":
-                kMeans();
-                break;
-            case "Median Cut":
-                medianCut();
-                break;
-            case "Frequency":
-                freqAnalysis();
-                break;
-            default:
-                kMeans();
+    const imgObject = new Image();
+    imgObject.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        canvas.width = imgObject.width;
+        canvas.height = imgObject.height;
+        ctx.drawImage(imgObject, 0, 0);
+        const img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        if (display.src) {
+            switch (choice) {
+                case "K-Means":
+                    kMeans(img, k);
+                    break;
+                case "Median Cut":
+                    medianCut(img, k);
+                    break;
+                case "Frequency":
+                    freqAnalysis(img, k);
+                    break;
+                default:
+                    console.log("Default case");
+            }
+        }
+        else {
+            console.log("Please upload an image first")
         }
     }
-    else {
-        console.log("Please upload an image first")
-    }
+    imgObject.src = display.src;
 }
 
 function displayImage(event) {
@@ -55,6 +106,7 @@ function displayImage(event) {
         };
         reader.readAsDataURL(file);
     }
+    clearColors();
 }
 
 function checkQuantity() {
